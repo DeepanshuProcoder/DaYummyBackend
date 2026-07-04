3bc297aec8c1c42000ba339f263dd036825b843b
DeepanshuProcoder <dk25051975@gmail.com>
2026-07-04 13:13:09 +0530

    Enhance OTP email sending with error handling
    Refactor sendOTP function to include error handling and logging.

--- utils/sendOTP.js
+++ utils/sendOTP.js
@@ -1,34 +1,44 @@
 const { BrevoClient } = require("@getbrevo/brevo");
 
 const brevo = new BrevoClient({
-  apiKey: process.env.BREVO_API_KEY,
+    apiKey: process.env.BREVO_API_KEY,
 });
 
 const sendOTP = async (email, name, otp) => {
-  await brevo.transactionalEmails.sendTransacEmail({
-    sender: {
-      name: "Da Yummy",
-      email: "dayummy25@gmail.com", // Your VERIFIED sender
-    },
-    to: [
-      {
-        email,
-        name,
-      },
-    ],
-    subject: "Food Website Email Verification",
-    htmlContent: `
-      <div style="font-family:Arial;padding:30px">
-        <h2>Hello ${name} 👋</h2>
-        <h3>Welcome to Da Yummy</h3>
-        <p>Your OTP is</p>
-        <h1 style="letter-spacing:8px;color:#ff6600">${otp}</h1>
-        <p>This OTP will expire in 5 minutes.</p>
-      </div>
-    `,
-  });
+    try {
+        const result = await brevo.transactionalEmails.sendTransacEmail({
+            sender: {
+                name: "Da Yummy",
+                email: "dayummy25@gmail.com", // Verified sender
+            },
 
-  console.log("✅ OTP Email Sent Successfully");
+            to: [
+                {
+                    email,
+                    name,
+                },
+            ],
+
+            subject: "Food Website Email Verification",
+
+            htmlContent: `
+            <div style="font-family:Arial;padding:30px">
+                <h2>Hello ${name} 👋</h2>
+                <h3>Welcome to Da Yummy</h3>
+                <p>Your OTP is</p>
+                <h1 style="letter-spacing:8px;color:#ff6600">${otp}</h1>
+                <p>This OTP expires in 5 minutes.</p>
+            </div>
+            `,
+        });
+
+        console.log("✅ Email sent");
+        console.log(result);
+    } catch (err) {
+        console.error("BREVO API ERROR");
+        console.error(err);
+        throw err;
+    }
 };
 
 module.exports = sendOTP;
